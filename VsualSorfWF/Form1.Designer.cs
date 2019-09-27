8a35604174554816174ea7580d7d507ed9f21d97
grs <ggg.roman101@gmail.com>
2019-09-27 11:36:58 +0300

    added button "exit"

--- VsualSorfWF/Form1.Designer.cs
+++ VsualSorfWF/Form1.Designer.cs
@@ -29,6 +29,7 @@
 		private void InitializeComponent()
 		{
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button2 = new System.Windows.Forms.Button();
             this.maxLim = new System.Windows.Forms.NumericUpDown();
             this.minLim = new System.Windows.Forms.NumericUpDown();
@@ -39,7 +40,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.maxLim)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.minLim)).BeginInit();
@@ -52,6 +53,7 @@
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.maxLim);
@@ -70,10 +72,25 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Настройки";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Сортировка пузырьком (Bubble sort)",
+            "Сортировка кучей (Heap sort)",
+            "Сортировка вставками (Insert sort)",
+            "Сортировка быстрая (quick sort)",
+            "Сортировка выборкой (selection sort)"});
+            this.comboBox1.Location = new System.Drawing.Point(7, 81);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(467, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(581, 31);
+            this.button2.Location = new System.Drawing.Point(581, 9);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(132, 44);
             this.button2.TabIndex = 10;
@@ -128,7 +145,7 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(720, 31);
+            this.button1.Location = new System.Drawing.Point(720, 9);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(132, 44);
@@ -196,20 +213,16 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // comboBox1
+            // button3
             // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Сортировка пузырьком (Bubble sort)",
-            "Сортировка кучей (Heap sort)",
-            "Сортировка вставками (Insert sort)",
-            "Сортировка быстрая (quick sort)",
-            "Сортировка выборкой (selection sort)"});
-            this.comboBox1.Location = new System.Drawing.Point(7, 81);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(467, 24);
-            this.comboBox1.TabIndex = 2;
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(731, 80);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 31);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -251,6 +264,7 @@
 		private System.Windows.Forms.NumericUpDown countElements;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
