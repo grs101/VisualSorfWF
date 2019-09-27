f87e38c6d3439f1ed136406be4265ce7bc64f7a9
grs <ggg.roman101@gmail.com>
2019-09-27 11:33:57 +0300

    added switch in method start for started sort

--- VsualSorfWF/Form1.cs
+++ VsualSorfWF/Form1.cs
@@ -7,6 +7,7 @@ namespace VisualSorfWF
 {
     public partial class Form1 : Form
     {
+        static int[] array;
         Thread MyThread1;
         public Form1()
         {
@@ -17,118 +18,23 @@ namespace VisualSorfWF
         private void button1_Click(object sender, EventArgs e)
         {
             Random random = new Random();
-
             int count = (int)countElements.Value;
             int min = (int)minLim.Value;
             int max = (int)(maxLim.Value + 1);
-            int [] array = new int[count];
+            array = new int[count];
             array = array.Select(x => random.Next(min, max)).ToArray();
-
             if (MyThread1 != null)
             {
-                if (!MyThread1.IsAlive)
+                if (!MyThread1.IsAlive) //проверка на выполнение потока. Не запускать, если он запущен
                 {
-                    switch (comboBox1.SelectedIndex)
-                    {
-                        case 0:
-                            {
-                                MyThread1 = new Thread(delegate ()
-                                {
-                                    BubbleSort.begin(array, ref pictureBox1, min, max);
-                                });
-                                MyThread1.Start();
-                                break;
-                            }
-                        case 1:
-                            {
-                                MyThread1 = new Thread(delegate ()
-                                {
-                                    HeapSort.begin(array, ref pictureBox1, min, max);
-                                });
-                                MyThread1.Start();
-                                break;
-                            }
-                        case 2:
-                            {
-                                MyThread1 = new Thread(delegate ()
-                                {
-                                    InsertSort.begin(array, ref pictureBox1, min, max);
-                                });
-                                MyThread1.Start();
-                                break;
-                            }
-                        case 3:
-                            {
-                                MyThread1 = new Thread(delegate ()
-                                {
-                                    QuickSort.begin(array, ref pictureBox1, 0, array.Length - 1, min, max);
-                                });
-                                MyThread1.Start();
-                                break;
-                            }
-                        case 4:
-                            {
-                                MyThread1 = new Thread(delegate ()
-                                {
-                                    SelectionSort.begin(array, ref pictureBox1, min, max);
-                                });
-                                MyThread1.Start();
-                                break;
-                            }
-                    }
+                    //главный запуск
+                    start(ref pictureBox1, ref comboBox1, ref MyThread1, min, max);
                 }
             }
             else
             {
-                
-                switch (comboBox1.SelectedIndex)
-                {
-                    case 0:
-                        {
-                            MyThread1 = new Thread(delegate ()
-                            {
-                                BubbleSort.begin(array, ref pictureBox1, min, max);
-                            });
-                            MyThread1.Start();
-                            break;
-                        }
-                    case 1:
-                        {
-                            MyThread1 = new Thread(delegate ()
-                            {
-                                HeapSort.begin(array, ref pictureBox1, min, max);
-                            });
-                            MyThread1.Start();
-                            break;
-                        }
-                    case 2:
-                        {
-                            MyThread1 = new Thread(delegate ()
-                            {
-                                InsertSort.begin(array, ref pictureBox1, min, max);
-                            });
-                            MyThread1.Start();
-                            break;
-                        }
-                    case 3:
-                        {
-                            MyThread1 = new Thread(delegate ()
-                            {
-                                QuickSort.begin(array, ref pictureBox1, 0, array.Length - 1, min, max);
-                            });
-                            MyThread1.Start();
-                            break;
-                        }
-                    case 4:
-                        {
-                            MyThread1 = new Thread(delegate ()
-                            {
-                                SelectionSort.begin(array, ref pictureBox1, min, max);
-                            });
-                            MyThread1.Start();
-                            break;
-                        }
-                }
+                //главный запуск
+                start(ref pictureBox1, ref comboBox1, ref MyThread1, min, max);
             }
         }
 
@@ -142,6 +48,59 @@ namespace VisualSorfWF
         private void Form1_Load(object sender, EventArgs e)
         {
             comboBox1.SelectedItem = comboBox1.Items[0];
+        }
+        //запуск сортировки с передачей ссылок на picturebox и combobox
+        static void start(ref PictureBox p, ref ComboBox c, ref Thread t, int min, int max)
+        {
+            PictureBox p1 = p; //создаю копию на ссылку picturebox, чтобы передать в Thread
+            switch (c.SelectedIndex)
+            {
+                case 0:
+                    {
+                        t = new Thread(delegate ()
+                        {
+                            BubbleSort.begin(array, ref p1, min, max);
+                        });
+                        t.Start();
+                        break;
+                    }
+                case 1:
+                    {
+                        t = new Thread(delegate ()
+                        {
+                            HeapSort.begin(array, ref p1, min, max);
+                        });
+                        t.Start();
+                        break;
+                    }
+                case 2:
+                    {
+                        t = new Thread(delegate ()
+                        {
+                            InsertSort.begin(array, ref p1, min, max);
+                        });
+                        t.Start();
+                        break;
+                    }
+                case 3:
+                    {
+                        t = new Thread(delegate ()
+                        {
+                            QuickSort.begin(array, ref p1, 0, array.Length - 1, min, max);
+                        });
+                        t.Start();
+                        break;
+                    }
+                case 4:
+                    {
+                        t = new Thread(delegate ()
+                        {
+                            SelectionSort.begin(array, ref p1, min, max);
+                        });
+                        t.Start();
+                        break;
+                    }
+            }
         }
     }
 }
